fbc4715e3d5f5d6cf9c4943747ab9e2e28c6f8b7
Antize <seba_0123@hotmail.com>
2019-09-16 23:58:50 +0200

    Delete AddAutoUpdateToStartup.bat

--- AddAutoUpdateToStartup.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit /b)
-
-reg add HKLM\Software\Microsoft\Windows\CurrentVersion\Run /v AntizeAutoUpdate /d %~dp0AutoUpdate.exe
-
-pause
